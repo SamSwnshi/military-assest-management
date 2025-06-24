51645266fc12df1d7e3d31b0857dcfd04f8f74fa
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-06-25 01:51:56 +0530

    name change

--- client/src/component/Layout/Layout.jsx
+++ client/src/component/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from "../Layout/Sidebar"
+import Sidebar from "./Sidebar"
 import Header from './Header';
 
 const Layout = () => {
