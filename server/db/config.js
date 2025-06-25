7e34cc36c52f70cf3f9de2a716d4ca4c2d62a0e6
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-06-25 15:45:50 +0530

    request

--- server/db/config.js
+++ server/db/config.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 
 const config = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL, {
+            heartbeatFrequencyMS: 60000,
+        });
         console.log("Connected to MONGO_DB")
     } catch (error) {
         console.log(error, "Error in Connecting to MongoDB_URL")
